cd22a83f1dc78b5e98b719925ed36345646e3984
charcodes24 <charcodes24@gmail.com>
2021-07-20 13:14:34 -0700

    commit tree 639d1e95d9eed42be67a4d550a2ecb029ac5d686

---
+++ lib/insert.sql
@@ -0,0 +1,80 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('website', 'javascript', 100, '2021-07-23', '2021-10-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('application', 'ruby', 200, '2021-06-23', '2021-09-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('calculator', 'react', 23, '2021-04-23', '2021-07-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('game', 'javascript', 250, '2021-02-23', '2021-05-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('brochure', 'ruby on rails', 123, '2021-03-23', '2021-09-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('database', 'python', 54, '2021-01-23', '2021-03-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('phone app', 'swift', 100, '2021-07-23', '2021-10-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('quiz app', 'react', 100, '2021-07-23', '2021-10-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('nested tables', 'SQL', 100, '2021-07-23', '2021-10-23');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('display', 'rack', 100, '2021-07-23', '2021-10-23');
+
+INSERT INTO users (name, age)
+    VALUES
+    ('charlene', 33), 
+    ('adam', 37),
+    ('ana', 59),
+    ('debbie', 33),
+    ('charlene', 34),
+    ('teresa', 33), 
+    ('iryna', 34),
+    ('alex', 33),
+    ('charlie', 64),
+    ('thomas', 42),
+    ('walter', 94), 
+    ('joni', 82),
+    ('scott', 60),
+    ('sarita', 60),
+    ('mark', 33),
+    ('aaron', 43), 
+    ('bruce', 68),
+    ('judy', 74),
+    ('grecia', 32),
+    ('katherine', 30);
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2), 
+(2, 20.00, 1, 3), 
+(3, 40.00, 1, 4), 
+(4, 50.00, 2, 3), 
+(5, 10.00, 3, 2), 
+(6, 20.00, 4, 4), 
+(7, 40.00, 5, 10), 
+(8, 60.00, 6, 10), 
+(9, 50.00, 7, 9), 
+(10, 700.00, 8, 8), 
+(11, 1000.00, 8, 7), 
+(12, 40.00, 9, 6), 
+(13, 50.00, 9, 3), 
+(14, 50.00, 10, 4), 
+(15, 24.00, 12, 1), 
+(16, 34.00, 11, 1), 
+(17, 12.00, 13, 6), 
+(18, 19.00, 14, 5), 
+(19, 20.00, 15, 5), 
+(20, 40.00, 16, 6), 
+(21, 35.50, 17, 7), 
+(22, 40.00, 18, 8), 
+(23, 60.00, 19, 9), 
+(24, 70.00, 20, 10), 
+(25, 100.00, 20, 4), 
+(26, 40.00, 19, 1), 
+(27, 20.00, 18, 6), 
+(28, 90.00, 17, 9), 
+(29, 230.00, 16, 6), 
+(30, 450.00, 15, 5);
+
+
+
+
+
+
